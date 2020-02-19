882855d80d10f2058c26a105d03134fa65f0b07a
Nirmalya Bandyopadhyay <nirmalya@broadinstitute.org>
2020-02-19 18:27:06 -0500

    Added some code for EM

--- NBinomEM.cpp
+++ NBinomEM.cpp
@@ -9,6 +9,9 @@
 #include <boost/math/special_functions/gamma.hpp>
 #include <boost/math/special_functions/digamma.hpp>
 
+#include <dlib/optimization.h>
+#include <dlib/global_optimization.h>
+
 #include "misc.hpp"
 
 
@@ -23,6 +26,8 @@ namespace po = boost::program_options;
 // C. A single negative binomial
 // D. Hurdle distributions ?
 
+typedef matrix<double,0,1> column_vector;
+
 class NBinomEMC {
 
     private:
@@ -36,6 +41,15 @@ class NBinomEMC {
         std::vector<long> sample_vec;
         double* mem_prob_mat; 
 
+        // For our model with one zero density + two NB density; the r_val[0]
+        // would be undefined. 
+        std::vector<double> prior_vec;
+        std::vector<double> p_vec;
+        std::vector<double> r_vec;
+
+        double get_nb_ll_by_k(int density_ind, double r_val);
+        double get_nb_ll_derivative_by_k(int density_ind, double r_val);
+
     public:
        
         NBinomEMC();
@@ -48,10 +62,167 @@ class NBinomEMC {
         void free_vars();
         double get_mem_prob(int density_ind, int sample_ind);
         void set_mem_prob(double mem_prob, int density_ind, int sample_ind);
-        double M_step_density_mean (int mix_ind);
-        double M_step_density_prob (int mix_ind);
+
+        double get_mix_ll_by_comp1(const column_vector& m);
+        double get_mix_ll_by_comp2(const column_vector& m);
+
+        double get_mix_ll_derivative_by_comp1(const column_vector& m);
+        double get_mix_ll_derivative_by_comp2(const column_vector& m);
+
+        double get_NB_density(long xi_val, double p_val, double r_val);
+
+        void do_E_step();
+        double get_LL();
         
 };
+
+double NBinomEMC::get_LL() {
+    double ll = 0;
+
+    for (int n = 0; n < sample_count; n++) {
+
+        long xn_val = sample_vec[n];
+        double comp_0_L = 0;
+
+        if (xn_val == 0) {
+            comp_0_L = 1;
+        } else {
+            comp_0_L = 0;
+        }
+
+        double comp_0_u = prior_vec[0] * comp_0_L;
+
+        double comp_1_L = get_NB_density(xn_val, p_vec[1], r_vec[1]); 
+        double comp_1_u = prior_vec[1] * comp_1_L;
+
+        double comp_2_L = get_NB_density(xn_val, p_vec[2], r_vec[2]);
+        double comp_2_u = prior_vec[2] * comp_2_L; 
+       
+        double l_n = comp_0_u + comp_1_u + comp_2_u;
+        double ll_n = log(l_n)
+        ll += ll_n;
+    }
+
+    return (ll);
+}
+
+void NBinomEMC::do_E_step() {
+
+    for (int n = 0; n < sample_count; n++) {
+
+        long xn_val = sample_vec[n];
+        double comp_0_L = 0;
+
+        if (xn_val == 0) {
+            comp_0_L = 1;
+        } else {
+            comp_0_L = 0;
+        }
+
+        double comp_0_u = prior_vec[0] * comp_0_L;
+
+        double comp_1_L = get_NB_density(xn_val, p_vec[1], r_vec[1]); 
+        double comp_1_u = prior_vec[1] * comp_1_L;
+
+        double comp_2_L = get_NB_density(xn_val, p_vec[2], r_vec[2]);
+        double comp_2_u = prior_vec[2] * comp_2_L; 
+       
+        double total_d = comp_0_u + comp_1_u + comp_2_u;
+
+        double comp_0_n = comp_0_u / total_d;
+        double comp_1_n = comp_1_u / total_d;
+        double comp_2_n = comp_2_u / total_d; 
+    
+        set_mem_prob(comp_0_n, 0, n);    
+        set_mem_prob(comp_1_n, 1, n);    
+        set_mem_prob(comp_2_n, 2, n);    
+    } 
+}
+
+double NBinomEMC::get_NB_density(long xi_val, double p_val, double r_val) {
+
+    double lval1 = lgamma(xi_val + r_val) -lgamma(xi_val + 1) -lgamma(r_val) + 
+            xi_val * log (lpval) + r_val * log(1-lpval);
+    double lval2 = exp(lval1);
+
+    return (lval2);
+
+}
+
+double NBinomEMC::get_nb_ll_by_k(int density_ind, double r_val) {
+    
+    if (!(density_ind == 1 || density_ind == 2)) {
+        std::string err_str = "Density index and function mismathch.";
+        throw std::runtime_error(err_str);
+    }
+
+    double lpval = p_vec[density_ind];
+    double lprior_val = prior_vec[density_ind];
+    double lprior_ln = math::log(lprior_val);
+
+    double total_ll = 0;
+
+    for (int j_ind = 0; j_ind < sample_vec.size(); j_ind) {
+        long xi_val = sample_vec[j_ind];
+
+        lval1 = lgamma(xi_val + r_val) -lgamma(xi_val + 1) -lgamma(r_val) + 
+            xi_val * log (lpval) + r_val * log(1-lpval);
+        lval2 = lprior_ln + lval1;
+        double lmem_prob = get_mem_prob(density_ind, j_ind);
+        double LL_xi = lmem_prob * lval2;
+        total_ll += LL_xi;
+    }         
+
+    return total_ll;
+}
+
+double NBinomEMC::get_mix_ll_by_comp1(const column_vector& m) {
+    const r_val = m(0);
+    double lval1 = get_nb_ll_by_k(1, r_val); 
+    return (lval1);
+}
+
+double NBinomEMC::get_mix_ll_by_comp2(const column_vector& m) {
+    const r_val = m(0);
+    double lval1 = get_nb_ll_by_k(2, r_val); 
+    return (lval1);
+}
+
+double NBinomEMC::get_nb_ll_derivative_by_k(int density_ind, double r_val) {
+    
+    if (!(density_ind == 1 || density_ind == 2)) {
+        std::string err_str = "Density index and function mismathch.";
+        throw std::runtime_error(err_str);
+    }
+
+    double lpval = p_vec[density_ind];
+
+    double total_derivative = 0;
+
+    for (int j_ind = 0; j_ind < sample_vec.size(); j_ind) {
+        long xi_val = sample_vec[j_ind];
+
+        lval1 = boost::math::digamma(xi_val + r_val) - 
+            boost::math::digamma(r_val) + log(1-lpval);
+        double lmem_prob = get_mem_prob(density_ind, j_ind);
+        double derivative_xi = lmem_prob * lval1;
+        total_derivative += derivative_xi;
+    }         
+
+    return total_derivative;
+}
+
+double NBinomEMC::get_mix_ll_derivative_by_comp1(const column_vector& m) {
+    const r_val = m(0);
+    double lval1 = get_nb_ll_derivative_by_k(1, r_val);
+    return (lval1);
+}
+
+double NBinomEMC::get_mix_ll_derivative_by_comp2(const column_vector& m) {
+    const r_val = m(0);
+    double lval1 = get_nb_ll_derivative_by_k(2, r_val);
+    return (lval1);
+}
 
 NBinomEMC::NBinomEMC() { 
     
